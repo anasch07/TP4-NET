58a58e88141e521030f42f1aeec0b4f6eb0b8c53
Anas Chaibi <anas.chaibi2@gmail.com>
2022-12-18 22:12:50 +0100

    tp 4 first commit work done

---
+++ TP4/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TP4.Data;
+using TP4.Models;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace TP4.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        UniversityContext universityContext = UniversityContext.Instantiate_UniversityContext();
+        Debug.WriteLine("UniversityContext is instantiated {0} times", UniversityContext.count);
+        List<Student> s = universityContext.Student.ToList();
+        
+        foreach (Student student in s)
+        {
+            Debug.WriteLine("----------------------------------------");
+            Debug.WriteLine("Student ID : {0}", student.id);
+            Debug.WriteLine("Student First Name : {0}", student.first_name);
+            Debug.WriteLine("Student Last Name : {0}", student.last_name);
+            Debug.WriteLine("Student Phone Number : {0}", student.phone_number);
+        }
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ TP4/Data/UniversityContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TP4.Models;
+
+namespace TP4.Data
+{
+    public class UniversityContext : DbContext
+    {
+        public DbSet<Student> Student { get; set; }
+        static private UniversityContext universityContextInstance = null;
+        static public int count = 0;
+        private UniversityContext(DbContextOptions o) : base(o)
+        {
+            count++;
+        }
+        static public UniversityContext Instantiate_UniversityContext()
+        {
+            if (universityContextInstance == null)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<UniversityContext>();
+                optionsBuilder.UseSqlite(@"Data Source=W:\Coding\INSAT\CodingGl3\.NET\TP4\2022GL3.NETFrameworkTP4-SQLiteDatabase.db");
+                universityContextInstance = new UniversityContext(optionsBuilder.Options);
+                return universityContextInstance;
+            }
+            else
+            {
+                return universityContextInstance;
+            }
+        }
+    }
+}
